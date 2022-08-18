b4965143c389544853ad5c50d32c5cb2832c91de
mitch145 <mitch.b@ownhome.com>
2022-08-18 15:34:49 +1000

    feat: build init

---
+++ src/lib/Typography.jsx
@@ -0,0 +1,130 @@
+import styled, { css } from "styled-components";
+
+export const HeroRegularCSS = css`
+    font-family: "Suisse Intl";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 80px;
+    line-height: 88px;
+    letter-spacing: -2px;
+    margin: 0;
+`;
+export const HeroRegular = styled.p`
+    ${HeroRegularCSS}
+`;
+
+export const Title2RegularCSS = css`
+    font-family: "Suisse Intl";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 48px;
+    line-height: 56px;
+    letter-spacing: -1.5px;
+    margin: 0;
+`;
+export const Title2Regular = styled.p`
+    ${Title2RegularCSS}
+`;
+
+export const BodyRegularCSS = css`
+    font-family: "Suisse Intl";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 20px;
+    letter-spacing: 0px;
+    margin: 0;
+`;
+export const BodyRegular = styled.p`
+    ${BodyRegularCSS}
+`;
+
+export const BodyMediumCSS = css`
+    font-family: "Suisse Intl";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 20px;
+    letter-spacing: 0px;
+    margin: 0;
+`;
+export const BodyMedium = styled.p`
+    ${BodyMediumCSS}
+`;
+
+export const LabelMediumCSS = css`
+    font-family: "Suisse Intl";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 16px;
+    letter-spacing: 0px;
+    margin: 0;
+`;
+export const LabelMedium = styled.p`
+    ${LabelMediumCSS}
+`;
+
+export const LabelRegularCSS = css`
+    font-family: "Suisse Intl";
+    font-style: normal;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 16px;
+    margin: 0;
+`;
+export const LabelRegular = styled.p`
+    ${LabelRegularCSS}
+`;
+
+export const CaptionMediumCSS = css`
+    font-family: "Suisse Intl";
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+    letter-spacing: 0px;
+    margin: 0;
+`;
+export const CaptionMedium = styled.p`
+    ${CaptionMediumCSS}
+`;
+
+export const Title1DisplayCSS = css`
+    font-family: "P22Mackinac";
+    font-weight: 500;
+    font-size: 64px;
+    line-height: 72px;
+    letter-spacing: -2px;
+    margin: 0;
+`;
+
+export const Title1Display = styled.p`
+    ${Title1DisplayCSS}
+`;
+
+export const Title3DisplayCSS = css`
+    font-family: "P22Mackinac";
+    font-weight: 500;
+    font-size: 40px;
+    line-height: 48px;
+    letter-spacing: -1.25px;
+    margin: 0;
+`;
+
+export const Title3Display = styled.p`
+    ${Title3DisplayCSS}
+`;
+
+export const Title4DisplayCSS = css`
+    font-family: "P22Mackinac";
+    font-weight: 500;
+    font-size: 32px;
+    line-height: 40px;
+    letter-spacing: -1px;
+    margin: 0;
+`;
+
+export const Title4Display = styled.p`
+    ${Title4DisplayCSS}
+`;
